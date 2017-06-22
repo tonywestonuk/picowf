6a32db126e92b26b1fc22f107b5e252509e8a5cb
tony.weston <tony.weston@EXE-PC-16.ad.mirada.local>
2017-06-22 17:17:02 +0100

    First commit - Example of remote component

---
+++ PicoTag/src/tonyfilters/Cors.java
@@ -0,0 +1,48 @@
+package tonyfilters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class Cors
+ */
+@WebFilter("/*")
+public class Cors implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Cors() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		((HttpServletResponse)response).addHeader("Access-Control-Allow-Origin", "*");
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
